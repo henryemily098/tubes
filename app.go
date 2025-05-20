dc646b36414326a2495b8d67f454e7b95e5bd47e
Henry Emily <henryandcharlie123@gmail.com>
2025-05-20 15:39:35 +0700

    Update app.go

--- app.go
+++ app.go
@@ -2,22 +2,100 @@ package main
 
 import "fmt"
 
-const NMAX int = 100
+const nTeamMAX int = 20
+const nMatchesMAX int = 760
 
 type Member struct {
-	id, name            string
+	id, name                           string
+	totalKill, totalDeath, totalAssist int
+}
+
+type MemberStats struct {
+	id                  string
 	kill, death, assist int
 }
 
 type Team struct {
-	id, name           string
-	win, lose          int
-	members            [5]Member
-	jadwalPertandingan [NMAX]int
+	id, name  string
+	win, lose int
+	members   [5]Member
 }
 
-var teams [NMAX]Team
+type Match struct {
+	date, month, year      int
+	tHomeId, tAwayId       string // Team ID
+	pHome, pAway           int    // Team Point
+	mHomeStats, mAwayStats [5]MemberStats
+}
+
+type tabTeams [nTeamMAX]Team
+type tabMatches [nMatchesMAX]Match
+
+var teams tabTeams
+var matches tabMatches
 var nTeams int
+var nMatches int
+
+func getIndexTeamFromName(name string) int {
+	var i, idx int
+
+	idx = -1
+	for i = 0; i < nTeams && idx < 0; i++ {
+		if teams[i].name == name {
+			idx = i
+		}
+	}
+
+	return idx
+}
+
+func getIndexTeamFromId(id string) int {
+	var i, idx int
+
+	idx = -1
+	for i = 0; i < nTeams && idx < 0; i++ {
+		if teams[i].id == id {
+			idx = i
+		}
+	}
+
+	return idx
+}
+
+func showStandingTable() {
+	var pass, idx, i int
+	var t tabTeams
+	var team Team
+
+	t = teams
+	pass = 1
+	for pass <= nTeams-1 {
+		idx = pass - 1
+		i = pass
+		for i < nTeams {
+			if (t[idx].win - t[idx].lose) < (t[i].win - t[i].lose) {
+				idx = i
+			}
+			i++
+		}
+		team = t[pass-1]
+		t[pass-1] = t[idx]
+		t[idx] = team
+		pass++
+	}
+
+	fmt.Println("+-----+------------+--------------------------------+--------+--------+")
+	fmt.Printf("| %-3s | %-10s | %-30s | %-6s | %-6s |\n", "Pos", "ID", "Nama Tim", "Menang", "Kalah")
+	fmt.Println("+-----+------------+--------------------------------+--------+--------+")
+
+	for i = 0; i < nTeams; i++ {
+		fmt.Printf("| %-3d | %-10s | %-30s | %-6d | %-6d |\n", i+1, t[i].id, t[i].name, t[i].win, t[i].lose)
+	}
+	if nTeams != 0 {
+		fmt.Println("+-----+------------+--------------------------------+--------+--------+")
+	}
+	fmt.Println()
+}
 
 func Print(message string, newLine bool) {
 	if newLine {
@@ -32,50 +110,431 @@ func clearTerminal() {
 	fmt.Print("\033[H")
 }
 
+// Opsi 1
+
+func createTeamName(team *Team) {
+	var name string
+	var isDuplicate bool
+	var i int
+
+	Print("Buatlah nama untuk tim yang sedang dibuat: ", false)
+	fmt.Scan(&name)
+
+	if len(name) < 2 && len(name) > 30 {
+		clearTerminal()
+		Print("Nama tim tidak boleh kurang dari 2 dan lebih dari 30 karakter!", true)
+		createTeamName(*&team)
+		return
+	}
+
+	isDuplicate = false
+	for i = 0; i < nTeams && !isDuplicate; i++ {
+		isDuplicate = name == teams[i].name
+	}
+
+	if isDuplicate {
+		clearTerminal()
+		Print("Nama "+name+" sudah digunakan oleh tim lain!", true)
+		createTeamName(*&team)
+	} else {
+		*&team.name = name
+		*&team.id = fmt.Sprintf("TE%d", nTeams+1)
+	}
+}
+
+func createTeamMembers(team *Team) {
+	var i int
+	var name string
+	Print("Buatlah anggota tim "+team.name+"!", true)
+	for i < 5 {
+		Print(fmt.Sprintf("Anggota %d: ", i+1), false)
+		fmt.Scan(&name)
+
+		if len(name) < 2 || len(name) > 15 {
+			Print("Nama anggota tidak boleh lebih dari 15 karakter atau kurang dari 2 karakter", true)
+		} else {
+			team.members[i].name = name
+			team.members[i].id = fmt.Sprintf("%s-ME%d", team.id, i+1)
+			i++
+		}
+	}
+}
+
+func createTeam() {
+	var team Team
+	createTeamName(&team)
+	createTeamMembers(&team)
+
+	teams[nTeams] = team
+	nTeams++
+
+	clearTerminal()
+	mainMenu()
+}
+
+// Opsi 2
+
+func selectTeam(idx *int) {
+	var name string
+	fmt.Println("Daftar Tim Dalam Turnamen")
+	showStandingTable()
+
+	Print("Tim mana yang hendak anda perbarui (ketik '0' untuk kembali): ", false)
+	fmt.Scan(&name)
+
+	if name != "0" {
+		*idx = getIndexTeamFromName(name)
+		clearTerminal()
+		if *idx == -1 {
+			Print("Tim yang hendak anda cari tidak ditemukan!", true)
+			selectTeam(*&idx)
+		}
+	} else {
+		*idx = -2
+	}
+}
+
+func selectUpdateOptions(idx int) {
+	var pick int
+	Print(fmt.Sprintf("(%s) Apa yang hendak anda perbarui?", teams[idx].name), true)
+	Print("1). Nama tim", true)
+	Print("2). Pertandingan", true)
+	Print("3). Kembali", true)
+	Print("Pilih: ", false)
+	fmt.Scan(&pick)
+
+	clearTerminal()
+	if pick < 1 || pick > 3 {
+		Print(fmt.Sprintf("Tidak ada opsi %d dalam pilihan!", pick), true)
+	} else {
+		switch pick {
+		case 1:
+			updateTeamName(idx)
+		case 2:
+			if nTeams == 1 {
+				Print("Anda tidak memperbarui jadwal pertandingan jika hanya ada 1 tim dalam turnamen!", true)
+			} else {
+				selectUpdateMatchOptions(idx)
+			}
+		}
+	}
+	if pick != 3 {
+		selectUpdateOptions(idx)
+	}
+}
+
+func updateTeamName(idx int) {
+	var name string
+	var i int
+	var isDuplicate bool
+
+	Print(fmt.Sprintf("Buat nama pengganti '%s': ", teams[idx].name), false)
+	fmt.Scan(&name)
+
+	clearTerminal()
+	if len(name) < 2 && len(name) > 30 {
+		Print("Nama tim tidak boleh kurang dari 2 dan lebih dari 30 karakter!", true)
+		updateTeamName(idx)
+	} else {
+		isDuplicate = false
+		for i = 0; i < nTeams && !isDuplicate; i++ {
+			if teams[idx].id != teams[i].id {
+				isDuplicate = name == teams[i].name
+			}
+		}
+
+		if isDuplicate {
+			Print("Nama "+name+" sudah digunakan oleh tim lain!", true)
+			updateTeamName(idx)
+		} else {
+			teams[idx].name = name
+		}
+	}
+}
+
+func addMatchDate(idx int, month, date, year *int) {
+	months := [12]string{"Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"}
+	Print("Tentukan tanggal (format: bulan tanggal tahun): ", false)
+	fmt.Scan(*&month, *&date, *&year)
+
+	clearTerminal()
+	if *month < 1 || *month > 12 {
+		Print("Anda memasukkan bulan yang tidak valid!", true)
+		addMatchDate(idx, *&month, *&date, *&year)
+		return
+	}
+
+	switch *month {
+	case 1, 3, 5, 7, 8, 10, 12:
+		if *date < 1 || *date > 31 {
+			Print(fmt.Sprintf("Anda hanya dapat memasukkan tanggal antara 1 hingga 31 untuk bulan %s!", months[*month-1]), true)
+			addMatchDate(idx, *&month, *&date, *&year)
+		}
+	case 4, 6, 9, 11:
+		if *date < 1 || *date > 30 {
+			Print(fmt.Sprintf("Anda hanya dapat memasukkan tanggal antara 1 hingga 30 untuk bulan %s!", months[*month-1]), true)
+			addMatchDate(idx, *&month, *&date, *&year)
+		}
+	case 2:
+		if *date < 1 || ((*date > 28 && *year%4 != 0) || (*date > 29 && *year%4 == 0)) {
+			if *year%4 == 0 {
+				Print(fmt.Sprintf("Anda hanya dapat memasukkan tanggal antara 1 hingga 29 untuk bulan %s!", months[*month-1]), true)
+			} else {
+				Print(fmt.Sprintf("Anda hanya dapat memasukkan tanggal antara 1 hingga 28 untuk bulan %s!", months[*month-1]), true)
+			}
+			addMatchDate(idx, *&month, *&date, *&year)
+		}
+	}
+}
+
+func addMatch(idx int) {
+	var i, j, k int
+	var totalMatches, pick int
+	var date, month, year int
+	var t tabTeams
+
+	Print("Daftar tim yang dapat menjadi lawan (maksimal pertemuan sebanyak 4x):", true)
+	for i = 0; i < nTeams; i++ {
+		if teams[idx].id != teams[i].id {
+			for j = 0; j < nMatches; j++ {
+				if (matches[j].tHomeId == teams[idx].id && matches[j].tAwayId == teams[i].id) || (matches[j].tHomeId == teams[i].id && matches[j].tAwayId == teams[idx].id) {
+					totalMatches++
+				}
+			}
+			Print(fmt.Sprintf("%d) %s (%dx pertemuan)", k+1, teams[i].name, totalMatches), true)
+			totalMatches = 0
+
+			t[k] = teams[i]
+			k++
+		}
+	}
+	Print("Pilih tim nomor berapa: ", false)
+	fmt.Scan(&pick)
+
+	if pick < 1 || pick > k {
+		clearTerminal()
+		Print("Masukan angka yang valid!", true)
+		addMatch(idx)
+		return
+	}
+
+	for i = 0; i < nMatches; i++ {
+		if (matches[i].tHomeId == teams[idx].id && matches[i].tAwayId == t[pick-1].id) || (matches[i].tHomeId == t[pick-1].id && matches[i].tAwayId == teams[idx].id) {
+			totalMatches++
+		}
+	}
+	if totalMatches >= 4 {
+		clearTerminal()
+		Print(teams[idx].name+" sudah berhadapan dengan "+t[pick-1].name+" sebanyak 4x, yang mana ini adalah jumlah maksimal!", true)
+		addMatch(idx)
+		return
+	}
+
+	matches[nMatches].tHomeId = teams[idx].id
+	matches[nMatches].tAwayId = t[pick-1].id
+	for i = 0; i < 5; i++ {
+		matches[nMatches].mHomeStats[i].id = teams[idx].members[i].id
+		matches[nMatches].mAwayStats[i].id = t[pick-1].members[i].id
+	}
+	clearTerminal()
+	addMatchDate(idx, &month, &date, &year)
+	matches[nMatches].month = month
+	matches[nMatches].date = date
+	matches[nMatches].year = year
+	nMatches++
+	clearTerminal()
+}
+
+func selectUpdateMatchOptions(idx int) {
+	var index, pick int
+	var i int
+
+	if nMatches != 0 {
+		fmt.Println("Daftar Pertandingan", teams[idx].name)
+		for i = 0; i < nMatches; i++ {
+			if matches[i].tHomeId == teams[idx].id {
+				index = getIndexTeamFromId(matches[i].tAwayId)
+			}
+			if matches[i].tAwayId == teams[idx].id {
+				index = getIndexTeamFromId(matches[i].tHomeId)
+			}
+
+			if (matches[i].tHomeId == teams[idx].id && matches[i].tAwayId == teams[index].id) || (matches[i].tHomeId == teams[index].id && matches[i].tAwayId == teams[idx].id) {
+				fmt.Printf("%s (%d) vs (%d) %s - ", teams[idx].name, matches[i].pHome, matches[i].pAway, teams[index].name)
+				fmt.Printf("%d/%d/%d", matches[i].date, matches[i].month, matches[i].year)
+				if matches[i].pHome == matches[i].pAway {
+					fmt.Print(" (Belum)\n")
+				} else {
+					fmt.Print("\n")
+				}
+			}
+		}
+		fmt.Println()
+	}
+
+	Print(fmt.Sprintf("(%s) Pilih salah-satu opsi di bawah ini:", teams[idx].name), true)
+	Print("1). Tambah Pertandingan", true)
+	Print("2). Perbarui Pertandingan", true)
+	Print("3). Hapus Pertandingan", true)
+	Print("4). Kembali", true)
+	Print("Pilih: ", false)
+	fmt.Scan(&pick)
+
+	clearTerminal()
+	if pick < 1 || pick > 4 {
+		Print("Kamu memilih pilihan di luar opsi!", true)
+	} else {
+		switch pick {
+		case 1:
+			addMatch(idx)
+		}
+	}
+	if pick != 4 {
+		selectUpdateMatchOptions(idx)
+	}
+}
+
+func updateTeam() {
+	var index int
+	selectTeam(&index)
+	clearTerminal()
+
+	if index != -2 {
+		selectUpdateOptions(index)
+		updateTeam()
+	} else {
+		mainMenu()
+	}
+}
+
+// Opsi 4
+
+func viewStandingDetails() {
+	var name string
+	var pick string
+	var index int
+
+	Print("Masukan nama tim mana yang hendak anda lihat statistik-nya: ", false)
+	fmt.Scan(&name)
+
+	index = getIndexTeamFromName(name)
+	if index < 0 {
+		Print("Sistem tidak menemukan tim bernama: "+name, true)
+		viewStandingDetails()
+		return
+	}
+	clearTerminal()
+
+	var members [5]Member
+	var temp Member
+	var pass, idx, i int
+
+	members = teams[index].members
+	pass = 1
+	for pass <= 4 {
+		idx = pass - 1
+		i = pass
+		for i < 5 {
+			if (members[idx].totalKill*3 + members[idx].totalAssist - members[idx].totalDeath) < (members[i].totalKill*3 + members[i].totalAssist - members[i].totalDeath) {
+				idx = i
+			}
+			i++
+		}
+		temp = members[pass-1]
+		members[pass-1] = members[idx]
+		members[idx] = temp
+		pass++
+	}
+	fmt.Println("Statistik Anggota", name)
+	fmt.Println("+------------+-----------------+--------+--------+--------+")
+	fmt.Printf("| %-10s | %-15s | %-6s | %-6s | %-6s |\n", "ID", "Anggota", "Kill", "Death", "Assist")
+	fmt.Println("+------------+-----------------+--------+--------+--------+")
+	for i = 0; i < 5; i++ {
+		fmt.Printf("| %-10s | %-15s | %-6d | %-6d | %-6d |\n", members[i].id, members[i].name, members[i].totalKill, members[i].totalDeath, members[i].totalAssist)
+	}
+	fmt.Println("+------------+-----------------+--------+--------+--------+")
+	fmt.Println()
+
+	Print("Ketik apapun untuk melanjutkan: ", false)
+	fmt.Scan(&pick)
+}
+
+func viewStanding() {
+	var pick string
+	fmt.Println("Klasemen Turnamen")
+	showStandingTable()
+	if nTeams != 0 {
+		Print("Apakah anda hendak melihat statistik tim lebih detail (ketik 'ya' untuk melihat, dan selain itu untuk kembali ke menu utama): ", false)
+		fmt.Scan(&pick)
+
+		if pick == "ya" || pick == "Ya" || pick == "yA" || pick == "YA" {
+			viewStandingDetails()
+			clearTerminal()
+			viewStanding()
+			return
+		}
+	} else {
+		Print("Belum ada tim dalam klasemen, ketik apapun untuk melanjutkan: ", false)
+		fmt.Scan(&pick)
+	}
+
+	clearTerminal()
+	mainMenu()
+}
+
 // Main Menu
 
-func mainMenu(selection *int) {
-	Print(fmt.Sprintf("Saat ini anda memiliki %d dari %d slot tim dalam klasemen!", nTeams, NMAX-nTeams), true)
+func mainMenu() {
+	var selection int
+	Print(fmt.Sprintf("Saat ini anda memiliki %d dari %d slot tim dalam klasemen!", nTeams, nTeamMAX-nTeams), true)
 	Print("Silakan pilih salah-satu opsi yang hendak anda lakukan:", true)
 	Print("1). Tambah Tim", true)
 	Print("2). Perbarui Tim", true)
 	Print("3). Hapus Tim", true)
 	Print("4). Lihat Klasemen", true)
-	Print("5). Lihat Statistik Tim dan Pemainnya", true)
-	Print("6). Keluar", true)
+	Print("5). Keluar", true)
 	Print("Pilihan anda: ", false)
-	fmt.Scan(*&selection)
+	fmt.Scan(&selection)
 
 	clearTerminal()
-	if *selection < 1 || *selection > 5 {
+	if selection < 1 || selection > 5 {
 		Print("-----------------------------------------------", true)
 		Print("Warning‼️", true)
-		Print(fmt.Sprintf("Tidak ada opsi %d dalam pilihan!", *selection), true)
+		Print(fmt.Sprintf("Tidak ada opsi %d dalam pilihan!", selection), true)
 		Print("-----------------------------------------------", true)
-		mainMenu(*&selection)
+		mainMenu()
+		return
 	}
 
-	if nTeams == NMAX && *selection == 1 {
+	if nTeams == 0 && (selection == 2 || selection == 3) {
+		Print("-----------------------------------------------", true)
+		Print("Warning‼️", true)
+		Print("Opsi 2 dan 3 belum tersedia jika belum ada tim yang terdaftar!", true)
+		Print("-----------------------------------------------", true)
+		mainMenu()
+		return
+	}
+
+	if nTeams == nTeamMAX && selection == 1 {
 		Print("-----------------------------------------------", true)
 		Print("Warning‼️", true)
 		Print("Kamu sudah tidak memiliki slot dalam klasemen untuk menambahkan tim!", true)
 		Print("-----------------------------------------------", true)
+		mainMenu()
+		return
 	}
-}
 
-// Opsi 1
-
-func createTeam() {
-
+	switch selection {
+	case 1:
+		createTeam()
+	case 2:
+		updateTeam()
+	case 4:
+		viewStanding()
+	}
 }
 
 func main() {
-	var s int
 	Print("Selamat datang di Turnamen anda!", true)
-	mainMenu(&s)
-
-	switch s {
-	case 1:
-		createTeam()
-	}
+	mainMenu()
 }
